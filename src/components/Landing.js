ae6c4e7acc3de0bd32dfdfd690dc604be007d5a9
Cindy Poon Yeung <cndpoon@gmail.com>
2018-09-05 17:23:04 -0400

    jsx for app and components

---
+++ src/components/Landing.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './../partials/_landing.scss';
+
+const Landing = () => {
+  return(
+    <section className="landing">
+      <h1 className="h1">Moon Shadow Cocktails</h1>
+
+      <p className="textblock">Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur, nihil ipsam eligendi, laboriosam, quae autem earum modi blanditiis sit ullam recusandae perspiciatis sint eum soluta odit? Minus reiciendis illo reprehenderit?</p>
+
+      <button className="btn">Inspire Me</button>
+    </section>
+  )
+}
+
+export default Landing;
